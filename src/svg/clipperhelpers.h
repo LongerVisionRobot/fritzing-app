255382ff6491a53ff608c41665a8b7e8a775ff27
Pei Jia <jiapei@longervision.com>
2025-07-20 11:22:04 -0700

    Compatible with clipper2, instead of clipper1 now

--- src/svg/clipperhelpers.h
+++ src/svg/clipperhelpers.h
@@ -25,96 +25,92 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 #define CLIPPERHELPERS_H
 
 
-#include <clipper.hpp>
+#include <clipper.h>
 #include <QPaintEngine>
 #include <fstream>
 
-inline ClipperLib::JoinType qtToClipperJoinType(Qt::PenJoinStyle style) {
+inline Clipper2Lib::JoinType qtToClipperJoinType(Qt::PenJoinStyle style) {
 	switch (style) {
 		case Qt::MiterJoin:
-			return ClipperLib::jtMiter;
-		case Qt::BevelJoin:
-			return ClipperLib::jtSquare;
-		case Qt::RoundJoin:
-			return ClipperLib::jtRound;
 		case Qt::SvgMiterJoin:
-			return ClipperLib::jtMiter;
+			return Clipper2Lib::JoinType::Miter;
+		case Qt::BevelJoin:
+			return Clipper2Lib::JoinType::Square;
+		case Qt::RoundJoin:
 		default:
-			return ClipperLib::jtRound;
+			return Clipper2Lib::JoinType::Round;
 	}
 }
 
-inline ClipperLib::EndType qtToClipperEndType(Qt::PenCapStyle style, bool open, bool fill) {
+inline Clipper2Lib::EndType qtToClipperEndType(Qt::PenCapStyle style, bool open, bool fill) {
 	if (open)
 		switch (style) {
 			case Qt::FlatCap:
-				return ClipperLib::etOpenButt;
+				return Clipper2Lib::EndType::Butt;
 			case Qt::SquareCap:
-				return ClipperLib::etOpenSquare;
+				return Clipper2Lib::EndType::Square;
 			case Qt::RoundCap:
-				return ClipperLib::etOpenRound;
 			default:
-				return ClipperLib::etOpenRound;
+				return Clipper2Lib::EndType::Round;
 		}
 	else if (fill)
-		return ClipperLib::etClosedPolygon;
+		return Clipper2Lib::EndType::Polygon;
 	else
-		return ClipperLib::etClosedLine;
+		return Clipper2Lib::EndType::Joined;
 }
 
-inline ClipperLib::PolyFillType qtToClipperFillType(QPaintEngine::PolygonDrawMode mode) {
+
+inline Clipper2Lib::FillRule qtToClipperFillType(QPaintEngine::PolygonDrawMode mode) {
 	switch (mode) {
 		case QPaintEngine::OddEvenMode:
-			return ClipperLib::pftEvenOdd;
+			return Clipper2Lib::FillRule::EvenOdd;
 		case QPaintEngine::WindingMode:
-			return ClipperLib::pftNonZero;
 		case QPaintEngine::ConvexMode:
-			return ClipperLib::pftNonZero;
 		case QPaintEngine::PolylineMode:
-			return ClipperLib::pftNonZero;
+		default:
+			return Clipper2Lib::FillRule::NonZero;
 	}
-	return ClipperLib::pftNonZero;
 }
 
-inline ClipperLib::Path pointsToClipper(const QPointF *points, int pointCount, QTransform matrix = QTransform()) {
-	ClipperLib::Path clipperPath;
+inline Clipper2Lib::Path64 pointsToClipper(const QPointF *points, int pointCount, QTransform matrix = QTransform()) {
+	Clipper2Lib::Path64 clipperPath;
 	for (int i = 0; i < pointCount; i++) {
 		const QPointF p2 = matrix.map(points[i]);
-		clipperPath << ClipperLib::IntPoint((ClipperLib::cInt) (p2.x()), (ClipperLib::cInt) (p2.y()));
+		clipperPath << Clipper2Lib::Point64((int64_t)(p2.x()), (int64_t)(p2.y()));
 	}
 	return clipperPath;
 }
 
-inline ClipperLib::Path polygonToClipper(QPolygonF poly, const QTransform &matrix = QTransform()) {
+inline Clipper2Lib::Path64 polygonToClipper(QPolygonF poly, const QTransform &matrix = QTransform()) {
 	return pointsToClipper(poly.data(), poly.size(), matrix);
 }
 
-inline ClipperLib::Paths polygonsToClipper(QList<QPolygonF> polys, const QTransform &matrix = QTransform()) {
-	ClipperLib::Paths paths;
+inline Clipper2Lib::Paths64 polygonsToClipper(QList<QPolygonF> polys, const QTransform &matrix = QTransform()) {
+	Clipper2Lib::Paths64 paths;
 	for (int i = 0; i < polys.size(); i++) {
 		paths << polygonToClipper(polys[i], matrix);
 	}
 	return paths;
 }
 
-inline QRectF boundingBox(ClipperLib::Paths paths) {
+inline QRectF boundingBox(Clipper2Lib::Paths64 paths) {
 	int minX = std::numeric_limits<int>::max();
 	int minY = std::numeric_limits<int>::max();
 	int maxX = std::numeric_limits<int>::min();
 	int maxY = std::numeric_limits<int>::min();
 	for (size_t i = 0; i < paths.size(); i++) {
 		for (size_t j = 0; j < paths[i].size(); j++) {
-			ClipperLib::IntPoint pt = paths[i][j];
-			minX = std::min(minX, (int) pt.X);
-			minY = std::min(minY, (int) pt.Y);
-			maxX = std::max(maxX, (int) pt.X);
-			maxY = std::max(maxY, (int) pt.Y);
+			const auto &pt = paths[i][j];
+			minX = std::min(minX, (int) pt.x);
+			minY = std::min(minY, (int) pt.y);
+			maxX = std::max(maxX, (int) pt.x);
+			maxY = std::max(maxY, (int) pt.y);
 		}
 	}
 	return QRectF(minX, minY, maxX - minX, maxY - minY);
 }
 
-inline QString clipperPathsToSVG(ClipperLib::Paths &paths, double clipperDPI, bool withSVGElement = true) {
+inline QString clipperPathsToSVG(Clipper2Lib::Paths64 &paths, double clipperDPI, bool withSVGElement = true) {
 	QRectF bounds = boundingBox(paths);
 	QStringList pSvg;
 
@@ -123,9 +119,9 @@ inline QString clipperPathsToSVG(ClipperLib::Paths &paths, double clipperDPI, bo
 	pSvg << QString("<path fill='black' stroke='none' stroke-width='0' d='");
 	for (size_t i = 0; i < paths.size(); i++) {
 		for (size_t j = 0; j < paths[i].size(); j++) {
-			ClipperLib::IntPoint pt = paths[i][j];
+			const auto &pt = paths[i][j];
 			pSvg << QString(j == 0 ? "M" : "L");
-			pSvg << QString("%1,%2 ").arg(pt.X - bounds.left()).arg(pt.Y - bounds.top());
+			pSvg << QString("%1,%2 ").arg(pt.x - bounds.left()).arg(pt.y - bounds.top());
 		}
 		pSvg << "Z";
 	}
@@ -135,15 +131,15 @@ inline QString clipperPathsToSVG(ClipperLib::Paths &paths, double clipperDPI, bo
 	return pSvg.join("");
 }
 
-inline void clipperPathsToSVGFile(ClipperLib::Paths &paths, double clipperDPI, QString fileName) {
+inline void clipperPathsToSVGFile(Clipper2Lib::Paths64 &paths, double clipperDPI, QString fileName) {
 	std::ofstream file(fileName.toStdString().c_str());
 	file << clipperPathsToSVG(paths, clipperDPI).toStdString();
 }
 
 inline QString imageToSVGPath(QImage &image, double res) {
-	// ClipperLib::Paths vectorized;
-	ClipperLib::Clipper cp;
-	ClipperLib::Paths lineSpans;
+	// Clipper2Lib::Paths64 vectorized;
+	Clipper2Lib::Clipper64 cp;
+	Clipper2Lib::Paths64 lineSpans;
 
 	// bool isIndexed = (image.format() == QImage::Format_Indexed8);
 
@@ -156,11 +152,11 @@ inline QString imageToSVGPath(QImage &image, double res) {
 				if (pix ^ (i == image.width() - 1)) {
 					startIndex = i;
 				} else {
-					ClipperLib::Path span;
-					span << ClipperLib::IntPoint(startIndex, j)
-						 << ClipperLib::IntPoint(i, j)
-						 << ClipperLib::IntPoint(i, j + 1)
-						 << ClipperLib::IntPoint(startIndex, j + 1);
+					Clipper2Lib::Path64 span;
+					span << Clipper2Lib::Point64(startIndex, j)
+						 << Clipper2Lib::Point64(i, j)
+						 << Clipper2Lib::Point64(i, j + 1)
+						 << Clipper2Lib::Point64(startIndex, j + 1);
 
 					lineSpans << span;
 				}
@@ -169,10 +165,9 @@ inline QString imageToSVGPath(QImage &image, double res) {
 		}
 	}
 
-	cp.AddPaths(lineSpans, ClipperLib::ptSubject, true);
-	// cp.AddPaths(vectorized, ClipperLib::ptClip, true);
-	ClipperLib::Paths result;
-	cp.Execute(ClipperLib::ctUnion, result, ClipperLib::pftNonZero, ClipperLib::pftNonZero);
+	cp.AddOpenSubject(lineSpans);  // new implementation
+	Clipper2Lib::Paths64 result;
+	cp.Execute(Clipper2Lib::ClipType::Union, Clipper2Lib::FillRule::NonZero, result);
 	return clipperPathsToSVG(result, res, false);
 }
 
